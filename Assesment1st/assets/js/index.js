4e26dba85683cd3fb48f9d73666ba344f179ff36
unknown <dixit.nabh@gmail.com>
2022-03-25 16:56:52 +0530

    moduleFirst

---
+++ Assesment1st/assets/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#info").click(function(){
+      
+    $("#res").toggle(200);
+   //   $("#info").toggleClass("explore");
+     // $("#info").html(pera);
+  });
+});
+$(document).ready(function(){
+  $("#read").click(function(){
+    $("#res1").toggle(200);
+  });
+});
+
